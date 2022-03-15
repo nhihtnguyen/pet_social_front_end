072740df075076d06869251e8b44b115dde599c1
zzf7ktx <zzf7ktx@gmail.com>
2022-03-15 16:38:48 +0700

    Add confirm dialog for report button

--- components/ReportButton.jsx
+++ components/ReportButton.jsx
@@ -2,8 +2,9 @@ import axiosClient from 'axiosSetup';
 import { useState } from 'react';
 import { FiImage, FiFileText, FiAlertCircle } from 'react-icons/fi';
 import { IoAlertCircleSharp, IoAlertCircleOutline } from 'react-icons/io5';
-import { Spinner } from 'react-bootstrap';
+import { Spinner, Modal, Button } from 'react-bootstrap';
 import { useEffect } from 'react';
+import Check from 'components/controls/Check';
 
 const MenuItem = ({ icon, tooltip, label, className, onClick, ...props }) => (
   <div
@@ -25,6 +26,11 @@ const ReportButton = ({ item, ...props }) => {
   const [toggleMore, setToggleMore] = useState(false);
   const [hasReport, setHasReport] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [reportType, setReportType] = useState('');
+  const handleCloseConfirm = () => {
+    setShowConfirm(false);
+  };
   const report = (type) => async () => {
     try {
       setToggleMore(false);
@@ -34,18 +40,19 @@ const ReportButton = ({ item, ...props }) => {
       console.log(error);
     }
   };
+  const handleChange = (e) => {
+    e.persist();
+
+    setReportType(() => e.target.value);
+  };
   useEffect(() => {
     return () => setToggleMore(false);
   }, []);
   return (
     <>
       <a
-        id='dropdownMenu4'
         className='cursor-pointer fw-600 text-grey-900 text-dark lh-26 font-xssss'
-        data-toggle='dropdown'
-        aria-haspopup='true'
-        aria-expanded='false'
-        onClick={() => setToggleMore(!toggleMore && !hasReport)}
+        onClick={() => setShowConfirm(!showConfirm && !hasReport)}
         {...props}
       >
         <span className='text-dark text-grey-900 btn-round-sm font-lg'>
@@ -56,27 +63,58 @@ const ReportButton = ({ item, ...props }) => {
           )}
         </span>
       </a>
-      <div
-        className={`${
-          toggleMore ? 'show' : ''
-        } dropdown-more-menu dropdown-menu dropdown-menu-end p-3 rounded-xxl border-0 shadow-lg top-100`}
-        style={{ right: 0, width: 'max-content' }}
-        aria-labelledby='dropdownMenu4'
+
+      <Modal
+        contentClassName='card rounded-xxl'
+        show={showConfirm}
+        onHide={handleCloseConfirm}
       >
-        <MenuItem
-          icon={<FiImage />}
-          label='Report image'
-          tooltip='Report image'
-          onClick={report('report_image')}
-        />
-        <MenuItem
-          icon={<FiFileText />}
-          label='Report caption'
-          tooltip='Report caption'
-          className='mt-2'
-          onClick={report('report_text')}
-        />
-      </div>
+        <Modal.Header className='text-dark'>
+          <Modal.Title>Report</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='text-dark'>
+          <div class='form-check'>
+            <input
+              class='form-check-input'
+              type='radio'
+              name='image'
+              id='image'
+              value='report_image'
+              onChange={handleChange}
+              checked={reportType === 'report_image'}
+            />
+            <label class='form-check-label' for='image'>
+              Image is not pet related
+            </label>
+          </div>
+          <div class='form-check'>
+            <input
+              class='form-check-input'
+              type='radio'
+              name='caption'
+              id='caption'
+              value='report_text'
+              onChange={handleChange}
+              checked={reportType === 'report_text'}
+            />
+            <label class='form-check-label' for='caption'>
+              Caption is not pet related
+            </label>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseConfirm}>
+            Close
+          </Button>
+          <Button
+            disabled={reportType == '' || typeof reportType == 'undefined'}
+            variant='danger'
+            onClick={report(reportType)}
+          >
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
